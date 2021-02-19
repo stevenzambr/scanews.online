83599cfd4b3a396321f9cede6456dbd10b5ec91d
Steven Zambrano <stevenzambr@gmail.com>
2021-02-19 01:59:19 +0100

    Small layout update

--- Back-End/dataFromApis.py
+++ Back-End/dataFromApis.py
@@ -52,8 +52,14 @@ file = open("currentsApiKey.txt")
 currentsApiKey = file.read()
 file.close()
 
-# Configurable parameter for currents latest news API
+# Currents API supported languages, countries and categories
 supportedLanguages = {"Arabic":"ar","Chinese":"zh","Dutch":"nl","English":"en","Finnish":"fi","French":"fr","German":"de","Hindi":"hi","Italian":"it","Japanese":"ja","Korean":"ko","Malay":"msa","Portuguese":"pt","Russian":"ru","Spanish":"es","Vietnamise":"vi"}
+supportedCountries = {"United State":"US","Taiwan":"TW","German":"DE","United Kingdom":"GB","China":"CN","India":"IN","Spain":"ES","Italy":"IT","Poland":"PL","Australia":"AU","Malaysia":"MY","Singapore":"SG","Canada":"CA","South Korea":"KR","Denmark":"DK","France":"FR","Belgium":"BE","Japan":"JP","Austria":"AT","Portugal":"PT","Philippines":"PH","Hong Kong":"HK","Argentina":"AR","Venezuela":"VE","Brazil":"BR","Finland":"FI","Indonedia":"ID","Vietnam":"VN","Mexico":"MX","Greece":"GR","Netherlands":"NL","Norway":"NO","New Zealand":"NZ","Russia":"RU","Saudi-Arabia":"SA","Switzerland":"CH","Thailand":"TH","United Arab Emirates":"AE","Ireland":"IE","Iran":"IR","Iraq":"IQ","Romania":"RO","Afghanistan":"AF","Zimbabwe":"ZW","Myanmar":"MM","Sweden":"SE","Peru":"PE","Panama":"PA","Egypt":"EG","Turkey":"TR","Israel":"IL","Czech Republic":"CZ","Bangladesh":"BD","Nigeria":"NG","Kenya":"KE","Chile":"CL","Uruguay":"UY","Ecuador":"EC","Serbia":"RS","Hungary":"HU","Slovenia":"SI","Gahana":"GH","Bolivia":"BO","Pakistan":"PK","Colombia":"CO","North Korea":"NK","Paraguay":"PY","Palestine":"PS","Estonia":"EE","Lebanon":"LB","Qatar":"QA","Kuwait":"KW","Cambodia":"KH","Nepal":"NP","Luxembourg":"LU","Bosnia":"BA","Europe":"EU","Asia":"ASIA","International":"INT"}
+supportedCategories = ["regional","technology","lifestyle","business","general","programming","science","entertainment","world","sports","finance","academia","politics","health","opinion","food","game","fashion","academic","crap","travel","culture","economy","environment","art","music","notsure","CS","education","redundant","television","commodity","movie","entrepreneur","review","auto","energy","celebrity","medical","gadgets","design","EE","security","mobile","estate","funny"]
+
+# Currents Latest News API
+
+# Configurable parameter for currents latest news API
 language = supportedLanguages["English"]
 
 # Currents API has 600 requests available per day (1 request every 2:24 minutes)
@@ -64,24 +70,27 @@ dataLatestNews = requests.get(url).json()
 with open('dataCurrentsLatestNews.json', 'w') as outfile:
     json.dump(dataLatestNews, outfile)
 
-# Configurable parameters for currents search API
-supportedLanguages = {"Arabic":"ar","Chinese":"zh","Dutch":"nl","English":"en","Finnish":"fi","French":"fr","German":"de","Hindi":"hi","Italian":"it","Japanese":"ja","Korean":"ko","Malay":"msa","Portuguese":"pt","Russian":"ru","Spanish":"es","Vietnamise":"vi"}
+# TODO: More testing of the Currents Search API 
 
+# Currents Search API
+
+# Configurable parameters for currents search API
 language = supportedLanguages["English"]
-# start_date = 
-# end_date = 
+start_date = "2021-02-18" # +'&start_date='+start_date
+end_date = "2021-02-19" # +'&end_date='+end_date
 # typeParameter =
-# country = 
-# category = 
+country = supportedCountries["International"] # +'&country='+country
+category = "world" # +'&category='+category
 # page_number =
-# domain =
+domain = "cnbc.com,nytimes.com"
+# domain_not =
 
 # Currents API has 600 requests available per day (1 request every 2:24 minutes)
-url = ('https://api.currentsapi.services/v1/search?language='+language+'&start_date'+'&apiKey='+currentsApiKey)
-dataLatestNews = requests.get(url).json()
+url = ('https://api.currentsapi.services/v1/search?language='+language+'&domain='+domain+'&apiKey='+currentsApiKey)
+dataSearch = requests.get(url).json()
 
-
-with open('dataCurrentsLatestNews.json', 'w') as outfile:
-    json.dump(dataLatestNews, outfile)
+# Save data search file locally
+with open('dataSearch.json', 'w') as outfile:
+    json.dump(dataSearch, outfile)
 
 
